15be6c67e725e3f83febe4b5cd50ac195642832b
Rajkumar-ojha <rajapptadinc@gmail.com>
2025-05-26 22:30:41 +0530

    1st commit

---
+++ dictEx2.py
@@ -0,0 +1,10 @@
+d1={"sno":100,"name":"raj","intmarks":{"cm":17,"c++":70},"extmarks":{"cm":90,"c++":90},"cname":"mukul"}
+print(d1,type(d1))
+for k in d1.keys():
+    print(k)
+for v in d1.values():
+    print(v)
+for its in d1.items():
+    print(its)
+for mukul,rupesh in d1.items():
+    print(mukul,"----->",rupesh)

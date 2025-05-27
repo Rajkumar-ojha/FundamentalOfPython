dc657ee7c0b1cdaedfc801b0e55c0e990ed9b63c
Rajkumar-ojha <rajapptadinc@gmail.com>
2025-05-27 15:15:24 +0530

    prime

---
+++ digittowordEx1.py
@@ -0,0 +1,17 @@
+words_upto_19=["","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+words_for_tens=["","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+n=int(input("enter a digit from 0 to 99:"))
+output=("")
+if (n==0):
+    output="zero"
+elif(n<=19):
+    output=words_upto_19[n]
+elif(n<=99):
+    output=words_for_tens[n//10]+""+words_upto_19[n%10]
+else:
+    output="plz enter a value from 0 to 99 only"
+print(output)
+
+
+
+

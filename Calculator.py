6c8bec688bd7916a1d76842c7de730d7cfe023f6
Rajkumar-ojha <rajapptadinc@gmail.com>
2025-07-25 22:21:15 +0530

    update calculator logic

--- Calculator.py
+++ Calculator.py
@@ -5,7 +5,7 @@ num2=float(input("enter the 2nd number:"))
 
 if op == "+":
     result = num1 + num2
-    print(round(result))
+    print(round(result,3))
 elif op == "-":
     result = num1 - num2
     print(round(result,3))
